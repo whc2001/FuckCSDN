612c35db4738abc1dfb739631adc12725703f76d
whc2001 <ianwang0122@outlook.com>
2018-12-05 19:03:11 -0500

    On Error Resume Next

--- FUCK CSDN.user.js
+++ FUCK CSDN.user.js
@@ -15,29 +15,40 @@ var body = document.getElementsByTagName("body")[0];
 
 var timer = setInterval(function()
 {
-    console.log("RUN");
-    var time = document.getElementById("check-adblock-time");
-    time.innerHTML = 2147483647;
-    // https://stackoverflow.com/questions/3141064/how-to-stop-all-timeouts-and-intervals-using-javascript
-    var maxTimerHandle = setInterval(function(){}, 1000);
-    for (var i = 0; i < maxTimerHandle; ++i)
-        if(i != timer)
-            clearInterval(i);
-    for(var j = head.children.length - 1; j >= 0; --j)
+    try
     {
-        var item = head.children[j];
-        if(item.tagName != "TITLE" && item.tagName != "META")
-            head.removeChild(item);
+        var time = document.getElementById("check-adblock-time");
+        time.innerHTML = 2147483647;
+        // https://stackoverflow.com/questions/3141064/how-to-stop-all-timeouts-and-intervals-using-javascript
+        var maxTimerHandle = setInterval(function(){}, 1000);
+        for (var i = 0; i < maxTimerHandle; ++i)
+            if(i != timer)
+                clearInterval(i);
     }
-    content = document.getElementById("content_views").cloneNode(true);
-    if(content !== null)
+    catch { }
+    try
     {
-        content.style.padding = "10px";
-        html.removeChild(body);
-        body = document.createElement("body");
-        body.appendChild(content);
-        html.appendChild(body);
+        for(var j = head.children.length - 1; j >= 0; --j)
+        {
+            var item = head.children[j];
+            if(item.tagName != "TITLE" && item.tagName != "META")
+                head.removeChild(item);
+        }
     }
+    catch { }
+    try
+    {
+        content = document.getElementById("content_views").cloneNode(true);
+        if(content !== null)
+        {
+            content.style.padding = "10px";
+            html.removeChild(body);
+            body = document.createElement("body");
+            body.appendChild(content);
+            html.appendChild(body);
+        }
+    }
+    catch { }
     if(++loop > 10)
         clearInterval(timer);
 }, 500);
